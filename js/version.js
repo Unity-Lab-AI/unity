df7bb2e5bd09d58bdc7bcf72b1f27b1a4c92b62d
GeeFourteen <gfourteen7525@gmail.com>
2026-04-14 06:56:56 -0600

    chore: stamp build for deploy

--- js/version.js
+++ js/version.js
@@ -11,5 +11,5 @@
  * (the push workflow in docs/PUSH_WORKFLOW.md wires this to git pre-push).
  */
 export const VERSION = '0.1.0';
-export const BUILD = '366d00c3-4c7e';
+export const BUILD = 'b5e9e886-c055';
 export const FULL = `${VERSION}+${BUILD}`;
